ecfdc71837aa6b18cca5f1c573510eaa3b408f8a
Juan Estrella <juan.estrella@finos.org>
2023-07-26 12:29:19 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -6,26 +6,27 @@ const readline = require('readline').createInterface({
 readline.question('input a password length ', size => {
   const num = size
 
-// create an array with each number from 1 to num
-const numbers = [];
-for (let i = 1; i <= num; i++) {
-  numbers.push(i);
-}
+  // create an array with each number from 1 to num
+  const numbers = [];
+  for (let i = 1; i <= num; i++) {
+    numbers.push(i);
+  }
 
-const length = numbers.length;
+  const length = numbers.length;
 
-function generatePassword(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+<>';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
-}
+  function generatePassword(length) {
+      let result = '';
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+<>';
+      const charactersLength = characters.length;
+      let counter = 0;
+      while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+      }
+      return result;
+  }
 
-console.log(generatePassword(length));
-  readline.close();
+  console.log(generatePassword(length));
+  
+readline.close();
 });
